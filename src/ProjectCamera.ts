469498970f7627e3cb075b36c507513a81795557
Adam Leis <adam.m.leis@gmail.com>
2026-06-29 23:22:51 -0400

    move camera init pos

--- src/ProjectCamera.ts
+++ src/ProjectCamera.ts
@@ -9,6 +9,11 @@ export class ProjectCamera {
     #canvas: HTMLCanvasElement;
     #cameraControls: OrbitControls;
     #cameraFolder: GUI;
+    #initPosition = {
+        x: 57,
+        y: 12,
+        z: 21,
+    };
 
     constructor(canvas: HTMLCanvasElement) {
         this.#canvas = canvas;
@@ -18,13 +23,21 @@ export class ProjectCamera {
             0.1,
             1000,
         );
-        this.instance.position.set(18, 3.5, 8.25);
+        this.instance.position.set(
+            this.#initPosition.x,
+            this.#initPosition.y,
+            this.#initPosition.z,
+        );
         this.#cameraControls = new OrbitControls(this.instance, canvas);
         this.#cameraControls.enableDamping = true;
 
         const gui = getGui();
         // might add camera controls to set position better for each spot
         this.#cameraFolder = gui.addFolder('Camera');
+        // position
+        this.#cameraFolder.add(this.instance.position, 'x', -100, 100).name('Position X');
+        this.#cameraFolder.add(this.instance.position, 'y', -100, 100).name('Position Y');
+        this.#cameraFolder.add(this.instance.position, 'z', -100, 100).name('Position Z');
         this.#cameraFolder.close();
     }
 
